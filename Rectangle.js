a1fe5c28971f9021b302d6901a2bae2eccf86640
Nick Berliner <nick.berliner96@gmail.com>
2020-12-28 16:24:30 +0100

    updates to readability

--- Rectangle.js
+++ Rectangle.js
@@ -1,46 +1,54 @@
 // Sets attributes for each rectangle
-class Rectangle {
-    constructor(x, y, width, height) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-    }
-    // Returns true or false based on whether coordings overlap
-    doesItOverlap(coords) {
-        let [x, y] = coords;
-        if (x >= this.x
-            && x < this.x + this.width
-            && y >= this.y
-            && y < this.y + this.height) {
-            return true;
-        }
-        return false;
-    }
-    calculateArea() {
-        return this.width * this.height;
-    }
-    // Returns a new Rectangle object from an array of coordinates
-    static build(arr) {
-
-        let xCoords = arr.map(coords => coords[0]).sort();
-        let yCoords = arr.map(coords => coords[1]).sort();
-
-        let [x1, , x2] = xCoords;
-        let [y1, , y2] = yCoords;
-
-        let width = Math.abs(x1 - x2);
-        let height = Math.abs(y1 - y2);
-        let x = Math.min(x1, x2);
-        let y = Math.min(y1, y2);
-
-        return new Rectangle(x, y, width, height);
-    }
+function Rectangle(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
 }
 
+// Iterates through each point, left-to-right, top-to-bottom
+Rectangle.prototype[Symbol.iterator] = function*() {
+    for ( let i = this.y; i < this.y + this.height; i++ ) {
+        for ( let j = this.x; j < this.x + this.width; j++ ) {
+            yield [i, j];
+        }
+    }
+};
 
+// Returns a new Rectangle object from an array of coordinates
+Rectangle.build = function(arr) {
 
+    let xCoords = arr.map(coords => coords[0]).sort();
+    let yCoords = arr.map(coords => coords[1]).sort();
 
+    let [x1, , x2] = xCoords;
+    let [y1, , y2] = yCoords;
+
+    let width = Math.abs(x1 - x2);
+    let height = Math.abs(y1 - y2);
+    let x = Math.min(x1, x2);
+    let y = Math.min(y1, y2);
+
+    return new Rectangle(x, y, width, height);
+};
+
+// Returns true or false based on whether coordings overlap
+Rectangle.prototype.doesItOverlap = function(coords) {
+    let [x, y] = coords;
+    if (   
+        x >= this.x 
+        && x < this.x + this.width 
+        && y >= this.y 
+        && y < this.y + this.height
+    ) {
+        return true;
+    }
+    return false;
+};
+
+Rectangle.prototype.calculateArea = function() {
+    return this.width * this.height;
+};
 
 function detect(strArr) {
     // turns string array into normal array
@@ -54,11 +62,11 @@ function detect(strArr) {
     let overlap = 0;
     for ( let coords of second ) {
       if ( first.doesItOverlap(coords) ) {
-        overlap ++
+        overlap ++;
       }
     }
 
-    if (overlap === 0) {
+    if ( overlap === 0 ) {
         return 0;
     }
     return overlap;
